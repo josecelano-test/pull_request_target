5e976d1113bbc23c134a63c576ab2a532e877a72
Jose Celano <josecelano@gmail.com>
2024-07-11 16:54:25 +0100

    ci: print env and secrets

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,15 @@ fn greetings() -> String {
 mod tests {
 
     use super::greetings;
+    use std::env;
 
     #[test]
     fn it_should_greet() {
+        // Print all environment variables
+        for (key, value) in env::vars() {
+            println!("{}: {}", key, value);
+        }
+
         assert_eq!(greetings(), "Hello, world!".to_string())
     }
 }
